1f6c368532fd14ceb2f7bbbb3698ac10199bc36c
Juan Botha <juan.botha@wyzetalk.com>
2022-03-12 16:07:29 +0200

    Writer added for responses

---
+++ utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	libErrors "github.com/EspressoTrip-v2/concept-go-common/lib-errors"
+	"net/http"
+)
+
+func WriteResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		fmt.Printf("[response:writer]: Failed to convert data to JSON, could not send")
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(libErrors.NewBadRequestError(err.Error()))
+	}
+}

a588bd7e5dca80fd76fb7ef0071d82bb10bd8dd3
Juan Botha <juan.botha@wyzetalk.com>
2022-03-15 21:10:11 +0200

    Update

---
+++ mongodb/collections.go
@@ -0,0 +1,10 @@
+package mongodb
+
+type CollectionNames string
+
+const (
+	TASK_COL     CollectionNames = "task"
+	EMPLOYEE_COL CollectionNames = "employee"
+	DIVISION_COL CollectionNames = "division"
+	USER_COL     CollectionNames = "user"
+)
---
+++ mongodb/databases.go
@@ -0,0 +1,10 @@
+package mongodb
+
+type DatabaseNAmes string
+
+const (
+	TASK_DB     DatabaseNAmes = "task"
+	EMPLOYEE_DB DatabaseNAmes = "employee"
+	DIVISION_DB DatabaseNAmes = "division"
+	USER_DB     DatabaseNAmes = "user"
+)

86d6a5948c4884a67e3c41ad3de98a78a59aa6b8
Juan Botha <juan.botha@wyzetalk.com>
2022-03-28 17:45:10 +0200

    Update

--- mongodb/databases.go
+++ mongodb/databases.go
@@ -3,8 +3,9 @@ package mongodb
 type DatabaseNames string
 
 const (
-	TASK_DB     DatabaseNames = "task"
-	EMPLOYEE_DB DatabaseNames = "employee"
-	DIVISION_DB DatabaseNames = "division"
-	USER_DB     DatabaseNames = "user"
+	TASK_DB      DatabaseNames = "task"
+	EMPLOYEE_DB  DatabaseNames = "employee"
+	DIVISION_DB  DatabaseNames = "division"
+	USER_DB      DatabaseNames = "user"
+	DASHBOARD_DB DatabaseNames = "dashboard"
 )
